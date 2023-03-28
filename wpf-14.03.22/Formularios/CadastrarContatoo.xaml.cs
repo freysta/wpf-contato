93ce2bfb55f8c53885523cc78d4a4b91efbce422
Freysta <99694214+freysta@users.noreply.github.com>
2023-03-28 11:42:31 -0400

    Adicionar arquivos de projeto.

---
+++ wpf-14.03.22/Formularios/CadastrarContatoo.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_14._03._22.Formularios
+{
+    /// <summary>
+    /// Interação lógica para CadastrarContatoo.xam
+    /// </summary>
+    public partial class CadastrarContatoo : Page
+    {
+        public CadastrarContatoo()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ wpf-14.03.22/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpf_14._03._22.Formularios;
+
+namespace wpf_14._03._22
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItemAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Menu Example - Version 1.0", "About");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarContatoo form = new CadastrarContatoo();
+            form.ShowsNavigationUI = true;
+            
+        }
+    }
+}
